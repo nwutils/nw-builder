66fd7addc0733aeea10352ce725a24253c96080c
Ayushman Chhabra <ayushmxn@outlook.com>
2022-05-15 19:33:41 -0400

    chore(demo): try to get a working demo

---
+++ demo/index.js
@@ -0,0 +1,24 @@
+const NW = require('../lib');
+
+const nw = new NW({
+    files: './**',
+    version: '0.64.0',
+
+    platforms: ['linux64']
+});
+
+nw.on('log', (msg) => {
+    console.log(msg)
+});
+
+// nw.build().catch((error) => {
+//     console.log(error)
+// });
+
+nw.run()
+    .then(() => {
+        console.log('Demo app is running!')
+    })
+    .catch((error) => {
+        console.log(error)
+    })
