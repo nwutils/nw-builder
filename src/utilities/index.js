8db97b3ffbc65965a63c3ba1f548adf2704e0d33
Ayushman Chhabra <ayushmxn@outlook.com>
2022-05-25 00:32:21 -0400

    refactor: platforms object

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,7 +1,9 @@
 const checkCache = require("./checkCache");
 const detectCurrentPlatform = require("./detectCurrentPlatform");
+const platforms = require("./platforms");
 
 module.exports = {
     checkCache,
     detectCurrentPlatform,
+    platforms,
 };
