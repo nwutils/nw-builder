a92ac7a79e6e1e1a346dfbe78838bc038c687dc5
Ayushman Chhabra <ayushmxn@outlook.com>
2022-05-25 19:25:03 -0400

    chore(demo): simplify example

--- example/nwapp/images/README.md
+++
@@ -1,7 +0,0 @@
-Any images used within the example app are stored in this directory.
-
----
-
-## See something which could be improved?
-
-Great, see [CONTRIBUTING.md](../../CONTRIBUTING.md).
---
+++ test/demo/index.js
@@ -0,0 +1,14 @@
+const NwBuilder = require("../../lib");
+
+const nw = new NwBuilder({
+  version: "0.64.1",
+  files: "./**",
+  macIcns: "./icon.icns",
+  macPlist: { mac_bundle_id: "myPkg" },
+});
+
+nw.on("log", (msg) => console.log("nw-builder", msg));
+
+nw.run()
+  .then(() => process.exit(0))
+  .catch((error) => console.log(error));
