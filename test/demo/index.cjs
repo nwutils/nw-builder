825bb5839577724d2aa60ddd6e5c1486cb8c2f20
Ayushman Chhabra <ayushmxn@outlook.com>
2022-06-25 03:02:40 -0400

    chore(tests): update demo

--- test/demo/index.cjs
+++ test/demo/index.cjs
@@ -1,10 +1,7 @@
 const NwBuilder = require("../../lib/index.cjs");
 
 const nw = new NwBuilder({
-  version: "0.64.1",
   files: "./**",
-  macIcns: "./icon.icns",
-  macPlist: { mac_bundle_id: "myPkg" },
 });
 
 nw.on("log", (msg) => console.log("nw-builder", msg));
