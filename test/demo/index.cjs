9cc97f7d6ccca6cd236add334cb271bc45afdaed
Ayushman Chhabra <ayushmxn@outlook.com>
2022-06-30 09:00:19 -0400

    feat(demo): exit process on app start

--- test/demo/index.cjs
+++ test/demo/index.cjs
@@ -1,13 +1,14 @@
 const NwBuilder = require("../../lib/index.cjs");
 
 const nw = new NwBuilder({
-  version: "0.64.1",
   files: "./**",
-  macIcns: "./icon.icns",
-  macPlist: { mac_bundle_id: "myPkg" },
 });
 
 nw.on("log", (msg) => console.log("nw-builder", msg));
+
+nw.on("appstart", () => {
+  process.exit(0);
+})
 
 nw.run()
   .then(() => process.exit(0))
