10bf6535e62de9e8efe22be4e00ad5f4ccf1b2b2
Ayushman Chhabra <ayushmxn@outlook.com>
2022-06-30 16:42:33 -0400

    fix(utils): use globbing to find package.json

--- src/utilities/validateOptions.js
+++ src/utilities/validateOptions.js
@@ -1,24 +1,32 @@
-import fs from "fs";
+import fs, { readFile } from "fs";
+import path from "path";
+
+import Glob from "simple-glob";
 import * as yup from "yup";
 
 const validateOptions = (options) => {
-  const optionsSchema = yup.object({
-    mode: "run" | "build" | "package",
-  });
+  const optionsSchema = yup.object({});
 
   let pkg = {};
 
-  if (
-    typeof options.files === "string" &&
-    fs.lstatSync(options.files).isDirectory() &&
-    fs.existsSync(`${options.files}/package.json`)
-  ) {
-    pkg = fs.readFileSync(`${options.files}/package.json`, "utf8");
-    pkg = JSON.parse(pkg);
+  if (typeof options.files === "string" || Array.isArray(options.files)) {
+    let matches = Glob(options.files);
+
+    if (matches.length === 0) {
+      console.error("package.json not found.");
+      process.exit(1);
+    }
+
+    matches.forEach((file) => {
+      if (file.match("package.json")) {
+        pkg = fs.readFileSync(`${file}`, "utf8");
+        pkg = JSON.parse(pkg);
+      }
+    });
   }
 
   if (pkg.nwbuild) {
-    pkg.nwbuild.files = options.files + "**";
+    pkg.nwbuild.files = options.files;
     return optionsSchema.validateSync(pkg.nwbuild);
   } else {
     return optionsSchema.validateSync(options);
