3a0aafd5f3e0d27f081e5413e476754e9c27a749
Ayushman Chhabra <ayushmxn@outlook.com>
2022-07-01 16:27:10 -0400

    feat(utils): update options schema

--- src/utilities/validateOptions.js
+++ src/utilities/validateOptions.js
@@ -5,7 +5,29 @@ import * as yup from "yup";
 
 const validateOptions = (options) => {
   const optionsSchema = yup.object({
-    mode: yup.string()
+    mode: yup.string(),
+    files: yup.string().required() || yup.array().of(yup.string()).required(),
+    flavor: yup.string(),
+    cacheDir: yup.string(),
+    platforms: yup.array().of(yup.string()),
+    currentPlatform: yup.string(),
+
+    appName: yup.string(),
+    appVersion: yup.string(),
+    buildDir: yup.string(),
+    buildType: yup.string(),
+    forceDownload: yup.boolean(),
+    macCredits: yup.string(),
+    macIcns: yup.string(),
+    macZip: yup.boolean(),
+    macPlist: yup.string() || yup.object(),
+    winVersionString: yup.object(),
+    winIco: yup.string(),
+    useRcedit: yup.boolean(),
+
+    zip: yup.boolean(),
+    zipOptions: yup.object(),
+    mergeZip: yup.boolean(),
   });
 
   let pkg = {};
