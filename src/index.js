54253e7675ee86125fcbc9b0f9839f00c2e5ea7a
Ayushman Chhabra <ayushmxn@outlook.com>
2022-07-02 00:53:49 -0400

    fix(constants): rename `logLevel` to `quiet` to prevent breaking change

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import Platforms from "./constants/Platforms";
 import checkCache from "./utilities/checkCache";
 import detectCurrentPlatform from "./utilities/detectCurrentPlatform";
 
-export { Platform, Platforms, detectCurrentPlatform, checkCache };
+export { Options, Platform, Platforms, detectCurrentPlatform, checkCache };
