3e5b99aae94858b0d8cb4cd0a482163e34f6097a
Ayushman Chhabra <ayushmxn@outlook.com>
2022-07-02 00:32:09 -0400

    chore: use explicit imports

--- src/constants/index.js
+++
@@ -1,7 +0,0 @@
-import Platform from "./Platform";
-import Platforms from "./Platforms";
-
-export {
-    Platform,
-    Platforms,
-};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-export { Platform, Platforms } from "./constants";
-export { detectCurrentPlatform, checkCache } from "./utilities";
+import Options from "./constants/Options";
+import Platform from "./constants/Platform";
+import Platforms from "./constants/Platforms";
+import checkCache from "./utilities/checkCache";
+import detectCurrentPlatform from "./utilities/detectCurrentPlatform";
+
+export { Platform, Platforms, detectCurrentPlatform, checkCache };
