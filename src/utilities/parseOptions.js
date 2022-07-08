3cb98e9b4846d3440e9ca078870152d8638ef926
Ayushman Chhabra <ayushmxn@outlook.com>
2022-07-07 23:28:47 -0400

    chore: format `parseOptions` function

--- src/utilities/parseOptions.js
+++ src/utilities/parseOptions.js
@@ -1,10 +1,10 @@
 const parseOptions = (options, defaultOptions) => {
-    for (const key of Object.keys(options)) {
-      if (defaultOptions.hasOwnProperty(key)) {
-        defaultOptions[key] = options[key];
-      }
+  for (const key of Object.keys(options)) {
+    if (defaultOptions.hasOwnProperty(key)) {
+      defaultOptions[key] = options[key];
     }
-    return defaultOptions;
-  };
+  }
+  return defaultOptions;
+};
 
-  export default parseOptions;
+export default parseOptions;
