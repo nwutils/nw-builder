d9deb1087957d5477504880fdc7502c4d6137753
Ayushman Chhabra <ayushmxn@outlook.com>
2022-08-09 01:30:46 -0400

    fix(tests): add back tests for detech current platform

---
+++ test/unit/detectCurrentPlatform.test.js
@@ -0,0 +1,59 @@
+import detectCurrentPlatform from "../../src/utilities/detectCurrentPlatform";
+import Arch from "../../src/constants/arch";
+import Platform from "../../src/constants/platform";
+
+const processEnv = { ...process.env };
+
+test("for OSX 32 platform", () => {
+  const process = {};
+  process.platform = "darwin";
+  process.arch = "x32";
+
+  expect(detectCurrentPlatform(process)).toBe("osx32");
+});
+
+test("for OSX 64 platform", () => {
+  const process = {};
+  process.platform = "darwin";
+  process.arch = "x64";
+
+  expect(detectCurrentPlatform(process)).toBe("osx64");
+});
+
+test("for Linux 32 platform", () => {
+  const process = {};
+  process.platform = "linux";
+  process.arch = "x32";
+
+  expect(detectCurrentPlatform(process)).toBe("linux32");
+});
+
+test("for Linux 64 platform", () => {
+  const process = {};
+  process.platform = "linux";
+  process.arch = "x64";
+
+  expect(detectCurrentPlatform(process)).toBe("linux64");
+});
+
+test("for Windows 32 platform", () => {
+  const process = {};
+  process.platform = "win32";
+  process.arch = "x32";
+  process.env = { ...processEnv };
+  process.env.PROCESSOR_ARCHITEW6432 = undefined;
+
+  expect(detectCurrentPlatform(process)).toBe("win32");
+});
+
+test("for Windows 64 platform", () => {
+  const process = {};
+  process.platform = "win32";
+  process.arch = "x64";
+  process.env = { ...processEnv };
+  process.env.PROCESSOR_ARCHITEW6432 = undefined;
+
+  expect(detectCurrentPlatform(process)).toBe("win64");
+});
+
+export {};
