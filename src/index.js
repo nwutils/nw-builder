ecd7875c64dc3bee2008b93ec55c54d65965f713
Ayushman Chhabra <ayushmxn@outlook.com>
2022-08-09 14:42:41 -0400

    fix(ci): remove unused vars

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-import download from "./api/download";
-import execute from "./api/execute";
+
 import run from "./api/run";
-import unzip from "./api/unzip";
 import Options from "./constants/Options";
-import Platform from "./constants/platform";
 import Platforms from "./constants/Platforms";
 import checkCache from "./utilities/checkCache";
 import checkPkgOptions from "./utilities/checkPkgOptions";
 import detectCurrentPlatform from "./utilities/detectCurrentPlatform";
-import getNwId from "./utilities/getNwId";
 import parseOptions from "./utilities/parseOptions";
 
 export {
