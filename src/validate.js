7a0b741ced7e814b42f860c90ec25c2c03535f28
Ayushman Chhabra <ayushmxn@outlook.com>
2022-09-01 00:36:46 +0530

    fix(tests): add tests for `mode`

--- src/validate.js
+++ src/validate.js
@@ -1,14 +1,17 @@
-import * as yup from "yup"
+import * as yup from "yup";
 
 const validate = async (options) => {
-    let optionsSchema = yup.object({
-        mode: yup.matches(/(run|build)/),
-        appDir: yup.string().required(),
-    });
+  let optionsSchema = yup.object({
+    mode: yup
+      .string()
+      .matches(/(run|build)/)
+      .required(),
+    appDir: yup.string().required(),
+  });
 
-    let validatedOptions = await optionsSchema.validate(options);
+  let validatedOptions = await optionsSchema.validate(options);
 
-    return validatedOptions;
+  return validatedOptions;
 };
 
 export default validate;
