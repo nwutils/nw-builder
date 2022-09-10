84c83be2107fa79ab51021b1bf6466b63607df29
Ayushman Chhabra <ayushmxn@outlook.com>
2022-09-09 21:15:24 -0400

    feat: validate remaining options

--- src/api/validate.js
+++ src/api/validate.js
@@ -1,12 +1,27 @@
 import * as yup from "yup";
 
 /**
- * A number, or a string containing a number.
+ * OptionsSchema
  * @typedef {Object} OptionsSchema
  * @property {string} files
  * @property {string} version
  * @property {"sdk" | "normal"} flavor
+ *
  * @property {string[]} platforms
+ * @property {string} appName
+ * @property {string} appVersion
+ * @property {string} cacheDir
+ * @property {string} buildDir
+ * @property {"default" | "versioned" | "timestamped"} buildType
+ * @property {string[]} argv
+ * @property {string} macCredits
+ * @property {string} macIcns
+ * @property {string | Object} macPlist
+ * @property {string} winIco
+ * @property {string} winVersionString
+ * @property {boolean} zip
+ * @property {Object} zipOptions
+ * @property {boolean} mergeZip
  */
 
 /**
@@ -20,6 +35,18 @@ const validate = async (options) => {
         version: yup.string().matches(/(latest|stable|^\d+\.\d+\.\d+$)/),
         flavor: yup.string().matches(/(sdk|normal)/),
         platforms: yup.array().of(yup.string().matches(/(linux|osx|win)(32|64)/)),
+        cacheDir: yup.string(),
+        buildDir: yup.string(),
+        buildType: yup.string().matches(/(default|versioned|timestamped)/),
+        argv: yup.array.of(yup.string()),
+        macCredits: yup.string(),
+        macIcns: yup.string(),
+        macPlist: yup.string(),
+        winIco: yup.string(),
+        winVersionString: yup.string(),
+        zip: yup.boolean(),
+        zipOptions: yup.object(),
+        mergeZip: yup.boolean(),
     })
 
     const error = await optionsSchema.validate(options);
