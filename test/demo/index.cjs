cefc9a50de57d93dbe507ae798b33ba10d1770e0
Ayushman Chhabra <ayushmxn@outlook.com>
2022-09-15 17:42:36 -0400

    chore: run prettier

--- test/demo/index.cjs
+++ test/demo/index.cjs
@@ -2,13 +2,13 @@ const NwBuilder = require("../../lib/index.cjs");
 
 const nw = new NwBuilder({
   files: "./**",
-  version: "0.67.1"
+  version: "0.67.1",
 });
 
 nw.run()
-.then((msg) => {
-  console.log(msg);
-})
-.catch((error) => {
-  console.log(error)
-});
+  .then((msg) => {
+    console.log(msg);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
