0c20d246af53613f8d45cf999741330ddf0e1717
Ayushman Chhabra <ayushmxn@outlook.com>
2022-11-16 23:25:48 -0500

    add winston for logging

--- src/bld/linuxCfg.js
+++ src/bld/linuxCfg.js
@@ -1,5 +1,7 @@
 import fs from "node:fs/promises";
 
+import { log } from "../log.js";
+
 /**
  * Generates a Desktop Entry file for Linux
  * https://specifications.freedesktop.org/desktop-entry-spec/latest/ar01s06.html
@@ -13,12 +15,13 @@ export async function setLinuxConfig(pkg, outDir) {
     let fileContent = `[Desktop Entry]\n`;
     Object.keys(pkg.linuxCfg).forEach((key) => {
       fileContent += `${key}=${pkg.linuxCfg[key]}\n`;
+      log.info(`Add ${key}=${pkg.linuxCfg[key]} to Desktop Entry File`);
     });
     let filePath = `${outDir}/${pkg.name}.desktop`;
     await fs.writeFile(filePath, fileContent);
   } else {
-    console.log(`[ WARN ] No LinuxCfg object found in srcDir/package.json`);
-    console.log("[ INFO ] The Desktop Entry file will not be generated");
-    console.log("[ INFO ] Consult the documentation for what properties to include");
+    log.warn("No LinuxCfg object found in srcDir/package.json`");
+    log.info("The Desktop Entry file will not be generated");
+    log.info("Consult the documentation for what properties to include");
   }
 }
---
+++ src/log.js
@@ -0,0 +1,24 @@
+import process from "node:process";
+import { createLogger, format, transports } from "winston";
+
+const { combine, timestamp, printf } = format;
+
+const customFormat = printf(({ level, message, timestamp}) => {
+  return `[ ${level.toUpperCase()} ] ${timestamp} ${message}`
+});
+
+export const log = createLogger({
+  format: combine(
+    timestamp(),
+    customFormat,
+  ),
+  transports: [
+    new transports.File({
+      filename: `${process.cwd()}/nwbuild.log`,
+      level: "silly",
+    }),
+    new transports.Console({
+      level: "silly",
+    }),
+  ],
+});
