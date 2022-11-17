2f9fb01ea20ddd4e476f4eb5cac8401e69ad2eb4
Ayushman Chhabra <ayushmxn@outlook.com>
2022-11-16 22:32:37 -0500

    add log level messages

--- src/bld/linuxCfg.js
+++ src/bld/linuxCfg.js
@@ -3,17 +3,21 @@ import fs from "node:fs/promises";
 /**
  * Generates a Desktop Entry file for Linux
  * https://specifications.freedesktop.org/desktop-entry-spec/latest/ar01s06.html
- * @param {Object} pkg srcDir's package.json as JSON
- * @param {string} outDir directory which stores build artifacts
- * @returns {undefined}
+ * @param  {object}    pkg     srcDir's package.json as JSON
+ * @param  {string}    outDir  directory which stores build artifacts
+ * @return {undefined}
  */
-export async function setLinuxConfig (pkg, outDir) {
-  let fileContent = `[Desktop Entry]\n`;
+export async function setLinuxConfig(pkg, outDir) {
   if (typeof pkg.linuxCfg === "object") {
+    let fileContent = `[Desktop Entry]\n`;
     Object.keys(pkg.linuxCfg).forEach((key) => {
       fileContent += `${key}=${pkg.linuxCfg[key]}\n`;
     });
+    let filePath = `${outDir}/${pkg.name}.desktop`;
+    await fs.writeFile(filePath, fileContent);
+  } else {
+    console.log(`[ WARN ] No LinuxCfg object found in srcDir/package.json`);
+    console.log("[ INFO ] The Desktop Entry file will not be generated");
+    console.log("[ INFO ] Consult the documentation for what properties to include");
   }
-  let filePath = `${outDir}/${pkg.name}.desktop`;
-  await fs.writeFile(filePath, fileContent);
-};
+}
