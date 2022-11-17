61f75197e705bcfb5a62b9806c8fc2c97512f4b2
Ayushman Chhabra <ayushmxn@outlook.com>
2022-11-16 22:18:42 -0500

    configure desktop entry file

--- src/bld/linuxCfg.js
+++ src/bld/linuxCfg.js
@@ -2,19 +2,18 @@ import fs from "node:fs/promises";
 
 /**
  * Generates a Desktop Entry file for Linux
- * https://specifications.freedesktop.org/desktop-entry-spec/latest/
+ * https://specifications.freedesktop.org/desktop-entry-spec/latest/ar01s06.html
  * @param {Object} pkg srcDir's package.json as JSON
  * @param {string} outDir directory which stores build artifacts
  * @returns {undefined}
  */
 export async function setLinuxConfig (pkg, outDir) {
-  let fileContent = `[Desktop Entry]
-    Name=${pkg.name}
-    Version=${pkg.version}
-    Exec=bash -c "${pkg.name} package.nw"
-    Type=Application
-    Terminal=false`;
-
+  let fileContent = `[Desktop Entry]\n`;
+  if (typeof pkg.linuxCfg === "object") {
+    Object.keys(pkg.linuxCfg).forEach((key) => {
+      fileContent += `${key}=${pkg.linuxCfg[key]}\n`;
+    });
+  }
   let filePath = `${outDir}/${pkg.name}.desktop`;
   await fs.writeFile(filePath, fileContent);
 };
