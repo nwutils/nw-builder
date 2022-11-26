e8c9f80ca935be1ffa576f49b59111ea6c13d966
Ayushman Chhabra <ayushmxn@outlook.com>
2022-11-26 00:33:30 -0500

    add code comments to set win config function

--- src/bld/winCfg.js
+++ src/bld/winCfg.js
@@ -2,11 +2,17 @@ import { rename } from "node:fs/promises";
 
 import rcedit from "rcedit";
 
+/**
+ * Windows specific configuration steps
+ *
+ * @param {object} pkg     The srcDir/package.json as a JSON
+ * @param {string} outDir  The directory to hold build artifacts
+ */
 const setWinConfig = async (pkg, outDir) => {
 
   await rename(`${outDir}/nw.exe`, `${outDir}/${pkg.name}.exe`);
 
-  return rcedit(`${outDir}/${pkg.name}.exe`, {
+  rcedit(`${outDir}/${pkg.name}.exe`, {
     "file-version": pkg.version,
     "product-version": pkg.version,
     "icon": pkg.icon,
