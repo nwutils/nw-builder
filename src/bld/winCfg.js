81545c575be1ae923c200f21bf542e9caeaf34e9
Ayushman Chhabra <ayushmxn@outlook.com>
2022-12-11 16:17:18 -0500

    link cfg docs

--- src/bld/winCfg.js
+++ src/bld/winCfg.js
@@ -4,14 +4,13 @@ import rcedit from "rcedit";
 
 /**
  * Windows specific configuration steps
+ * https://learn.microsoft.com/en-gb/windows/win32/menurc/versioninfo-resource?redirectedfrom=MSDN#string-name
  *
  * @param {object} pkg     The srcDir/package.json as a JSON
  * @param {string} outDir  The directory to hold build artifacts
  */
 const setWinConfig = async (pkg, outDir) => {
   await rename(`${outDir}/nw.exe`, `${outDir}/${pkg.name}.exe`);
-
-  // https://learn.microsoft.com/en-gb/windows/win32/menurc/versioninfo-resource?redirectedfrom=MSDN#string-name
   await rcedit(`${outDir}/${pkg.name}.exe`, {
     "file-version": pkg.version,
     "product-version": pkg.version,
