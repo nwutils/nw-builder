cd178ea206f62c5f33eb931919a434e24ff32517
Ayushman Chhabra <ayushmxn@outlook.com>
2022-12-22 18:30:38 -0500

    add windows demo to ci

--- test/demo/bld.cjs
+++ test/demo/bld.cjs
@@ -1,33 +1,41 @@
+const { platform } = require("node:process");
+
 const { nwbuild } = require("nw-builder");
 
 const bld = async () => {
-  // await nwbuild({
-  //   srcDir: "./nwapp",
-  //   mode: "build",
-  //   version: "0.70.1",
-  //   flavour: "normal",
-  //   platform: "linux",
-  //   arch: "x64",
-  //   outDir: "./build/nix",
-  // });
-  // await nwbuild({
-  //   srcDir: "./nwapp",
-  //   mode: "build",
-  //   version: "0.70.1",
-  //   flavour: "normal",
-  //   platform: "osx",
-  //   arch: "x64",
-  //   outDir: "./build/osx",
-  // });
-  await nwbuild({
-    srcDir: "./nwapp",
-    mode: "build",
-    version: "0.70.1",
-    flavour: "normal",
-    platform: "win",
-    arch: "x64",
-    outDir: "./build/win",
-  });
+  if (platform === "linux") {
+    await nwbuild({
+      srcDir: "./nwapp",
+      mode: "build",
+      version: "0.70.1",
+      flavour: "normal",
+      platform: "linux",
+      arch: "x64",
+      outDir: "./build/nix",
+    });
+  }
+  if (platform === "darwin") {
+    await nwbuild({
+      srcDir: "./nwapp",
+      mode: "build",
+      version: "0.70.1",
+      flavour: "normal",
+      platform: "osx",
+      arch: "x64",
+      outDir: "./build/osx",
+    });
+  }
+  if (platform === "win32") {
+    await nwbuild({
+      srcDir: "./nwapp",
+      mode: "build",
+      version: "0.70.1",
+      flavour: "normal",
+      platform: "win",
+      arch: "x64",
+      outDir: "./build/win",
+    });
+  }
 };
 
 bld();
