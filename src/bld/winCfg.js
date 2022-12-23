2bd6ed5dde064768236a4a8f9be5d08b6d1b1d35
Ayushman Chhabra <ayushmxn@outlook.com>
2022-12-23 17:31:47 -0500

    dont run rcedit for windows builds

--- src/bld/winCfg.js
+++ src/bld/winCfg.js
@@ -12,17 +12,17 @@ const setWinConfig = async (pkg, outDir) => {
   await rename(`${outDir}/nw.exe`, `${outDir}/${pkg.name}.exe`);
 
   // https://learn.microsoft.com/en-gb/windows/win32/menurc/versioninfo-resource?redirectedfrom=MSDN#string-name
-  await rcedit(`${outDir}/${pkg.name}.exe`, {
-    "file-version": pkg.version,
-    "product-version": pkg.version,
-    "icon": pkg.icon,
-    "version-string": {
-      FileDescription: pkg.description,
-      LegalCopyright: pkg.copyright,
-      ProductName: pkg.name,
-      OriginalFilename: pkg.name,
-    },
-  });
+  // await rcedit(`${outDir}/${pkg.name}.exe`, {
+  //   "file-version": pkg.version,
+  //   "product-version": pkg.version,
+  //   "icon": pkg.icon,
+  //   "version-string": {
+  //     FileDescription: pkg.description,
+  //     LegalCopyright: pkg.copyright,
+  //     ProductName: pkg.name,
+  //     OriginalFilename: pkg.name,
+  //   },
+  // });
 };
 
 export { setWinConfig };
