37d020478004159e0293ecf6c4317bc7158f8053
Ayushman Chhabra <ayushmxn@outlook.com>
2022-12-23 18:25:15 -0500

    format

--- test/e2e/osx.cjs
+++ test/e2e/osx.cjs
@@ -1,11 +1,11 @@
 const { nwbuild } = require("nw-builder");
 
 nwbuild({
-    srcDir: "./nwapp",
-    mode: "build",
-    version: "0.70.1",
-    flavour: "normal",
-    platform: "linux",
-    arch: "x64",
-    outDir: "./build/osx",
-  });
+  srcDir: "./nwapp",
+  mode: "build",
+  version: "0.70.1",
+  flavour: "normal",
+  platform: "linux",
+  arch: "x64",
+  outDir: "./build/osx",
+});
