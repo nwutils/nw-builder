504b1d8fbf556123df5a0992c1b563650b2e9634
Ayushman Chhabra <ayushmxn@outlook.com>
2022-12-30 16:34:44 -0500

    update jsdocs

--- src/run/execute.js
+++ src/run/execute.js
@@ -1,22 +1,32 @@
-import child_process from "node:child_process";
+import { spawn } from "node:child_process";
 
+import { log } from "../log.js";
+
+/**
+ * Executes the NW.js process
+ * @param  {string}             srcDir  The source directory
+ * @param  {string}             nwPath  The path to the NW.js executable
+ * @param  {string}             argv    The arguments to pass to the NW.js process
+ * @return {Promise<undefined>}         The exit code of the NW.js process
+ * @throws {Error} - If the NW.js process fails to start
+ */
 const execute = (srcDir, nwPath, argv) => {
   return new Promise((resolve, reject) => {
     // It is assumed that the first glob pattern p contains the package.json at p/package.json
     srcDir = srcDir.split(" ")[0];
     srcDir = srcDir.replace(/\*[/*]*/, "");
-    const nwProcess = child_process.spawn(nwPath, [srcDir.concat(argv)], {
+    const nwProcess = spawn(nwPath, [srcDir.concat(argv)], {
       detached: true,
       windowsHide: true,
     });
 
     nwProcess.on("close", () => {
-      resolve(0);
+      resolve();
     });
 
     nwProcess.on("error", (error) => {
-      console.log(error);
-      reject(1);
+      log.error(error);
+      reject(error);
     });
   });
 };
