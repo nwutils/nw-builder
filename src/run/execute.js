8d5e0e2d82ed3daf4e3d56a33ec4a3a818ffaa8a
Ayushman Chhabra <ayushmxn@outlook.com>
2022-12-30 15:59:14 -0500

    format

--- src/run/execute.js
+++ src/run/execute.js
@@ -4,7 +4,7 @@ const execute = (srcDir, nwPath, argv) => {
   return new Promise((resolve, reject) => {
     // It is assumed that the first glob pattern p contains the package.json at p/package.json
     srcDir = srcDir.split(" ")[0];
-    srcDir = srcDir.replace(/\*[/*]*/, "")
+    srcDir = srcDir.replace(/\*[/*]*/, "");
     const nwProcess = child_process.spawn(nwPath, [srcDir.concat(argv)], {
       detached: true,
       windowsHide: true,
