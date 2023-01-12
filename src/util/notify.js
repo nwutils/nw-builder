5fa358292a90fbb8b06342b244a6a72e10ba75d3
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2023-01-12 18:14:21 -0300

    fix: correct notify util (#767)

--- src/util/notify.js
+++ src/util/notify.js
@@ -1,16 +1,15 @@
-import { resolve } from "node:path";
-import { readFile } from "node:fs/promises";
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
 
 import updateNotifier from "update-notifier";
 
+
 /**
  * Notify the user if there is a new version of the package available.
- *
- * @return {Promise<void>}
- */
+*
+* @return {Promise<void>}
+*/
 export const notify = async () => {
-  const packageJson = JSON.parse(
-    await readFile(resolve("..", "..", "package.json")),
-  );
+  const packageJson = require("../../package.json");
   updateNotifier({ pkg: packageJson }).notify();
 };
