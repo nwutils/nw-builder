7b738104fa84e954e0ddd32bc3118428890d61cf
Ayushman Chhabra <ayushmxn@outlook.com>
2023-02-15 03:53:37 -0500

    use nw v0.73.0

--- e2e/mode.js
+++ e2e/mode.js
@@ -16,7 +16,7 @@ export function mode() {
         let nwOptions = {
             srcDir: "./e2e/app/*",
             mode: "build",
-            version: "0.72.0",
+            version: "0.73.0",
             flavor: "sdk",
             outDir: "./e2e/out",
             cacheDir: "./e2e/tmp"
