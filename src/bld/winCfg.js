8c49de049d7a674b6c8bb2c04b47851c8ed51dca
Jiayu Wang <jiayu.wang@emeraldcloudlab.com>
2023-03-09 17:09:27 -0600

    Use resolve() to get file path for rename and rcedit on Windows

--- src/bld/winCfg.js
+++ src/bld/winCfg.js
@@ -37,17 +37,20 @@ const setWinConfig = async (app, outDir) => {
   });
 
   try {
-    await rename(`${outDir}/nw.exe`, `${outDir}/${app.name}.exe`);
-
-    await rcedit(`${outDir}/${app.name}.exe`, {
-      "file-version": app.version,
-      "icon": app.icon,
-      "product-version": app.version,
-      "version-string": versionString,
-    });
+    const outDirAppExe = resolve(outDir, `${app.name}.exe`);
+    await rename(resolve(outDir, 'nw.exe'), outDirAppExe);
+    await rcedit(
+      outDirAppExe,
+      {
+        "file-version": app.version,
+        "icon": app.icon,
+        "product-version": app.version,
+        "version-string": versionString,
+      },
+    );
   } catch (error) {
     log.warn(
-      "Unable to modify EXE. Ensure WINE is installed or build in Windows",
+      "Renaming EXE failed or unable to modify EXE. If it's the latter, ensure WINE is installed or build in Windows",
     );
     log.error(error);
   }
