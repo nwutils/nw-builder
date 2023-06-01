ce0448abb40452f8d38dc169290b519b3e20c3e8
Ayushman Chhabra <info@ayushmanchhabra.com>
2023-06-02 01:39:35 +0530

    test: migrate from Jest to Node Test runner

---
+++ test/unit/cache.js
@@ -0,0 +1,14 @@
+import { equal } from "node:assert";
+import { describe, it } from "node:test";
+
+import { isCached } from "../../src/util/cache.js";
+
+describe("cache tests", () => {
+  it("directory does not exist", async () => {
+    equal(await isCached(""), false);
+  });
+
+  it("directory exists", async () => {
+    equal(await isCached("./e2e/fixture/cacheDir"), true);
+  });
+});
