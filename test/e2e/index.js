9b8e85a0dbeb79f5ffd23a7cb7d502e279d9ccb1
ayushmanchhabra <ayushmanchhabra@users.noreply.github.com>
2023-06-08 18:47:25 +0000

    deploy: 49458106ba140ccd8cc2781b92146eff8ba970ea

--- test/e2e/index.js
+++
@@ -1,7 +0,0 @@
-import { mode } from "./mode.js";
-
-const test = async () => {
-  await mode();
-};
-
-test();
--- test/unit/index.js
+++
@@ -1,5 +0,0 @@
-import * as archTests from "./arch.js";
-import * as cacheTests from "./cache.js";
-import * as parseTests from "./parse.js";
-import * as platformTests from "./platform.js";
-import * as valTests from "./validate.js";
