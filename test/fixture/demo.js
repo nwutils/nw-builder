d65bc8932c1563eafa366afcc75b39aecf511e0d
Ayushman Chhabra <info@ayushmanchhabra.com>
2023-07-07 00:23:03 +0530

    test: add manifest url for osx unofficial build

--- test/fixture/demo.js
+++ test/fixture/demo.js
@@ -7,6 +7,8 @@ await nwbuild({
   platform: "osx",
   arch: "arm64",
   downloadUrl: "https://github.com/corwin-of-amber/nw.js/releases/download",
+  manifestUrl:
+    "https://raw.githubusercontent.com/nwutils/nw-builder/main/src/util/osx.arm.versions.json",
   outDir: "out",
   glob: false,
   app: {
