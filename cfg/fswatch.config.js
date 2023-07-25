133b4b28b91e6f459d4dfbe01605062452ac8011
Ayushman Chhabra <info@ayushmanchhabra.com>
2023-07-25 21:33:11 +0530

    docs: convert jsdoc to md on hmr

---
+++ cfg/fswatch.config.js
@@ -0,0 +1,16 @@
+import { exec } from 'node:child_process';
+import { watch } from 'node:fs/promises';
+
+import { log } from '../src/log.js';
+
+try {
+    const watcher = await watch("src", { recursive: true });
+
+    for await (const event of watcher) {
+        if (event) {
+            exec("node cfg/jsdoc.config.cjs");
+        }
+    }
+} catch (e) {
+    log.error(e);
+}
