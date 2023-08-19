a24d0b5af30d30a724ea4c49f41ef692780c9e7b
Ayushman Chhabra <ayushmanchhabra@users.noreply.github.com>
2023-08-18 22:17:45 -0400

    fix: build mode w/o handling for platforms

--- test/demo.cjs
+++ test/demo.cjs
@@ -1,8 +1,23 @@
+const fs = require("node:fs");
+const { resolve } = require("node:path");
+
 const NwBuilder = require("../lib/index.cjs");
 
 const nw = new NwBuilder({
-    files: "test/demo/*",
+    files: [
+        "package.json",
+        "test/demo/*"
+    ],
     version: "0.67.1",
 });
 
-nw.run();
+nw.build();
+
+let NW_manifest = JSON.parse(fs.readFileSync("package.json", { encoding: "utf8" }));
+
+NW_manifest.main = "test/demo/index.html";
+
+const manifestPath = resolve("build", "nw-builder", "win64", "package.nw", "package.json");
+const manifestData = JSON.stringify(NW_manifest)
+
+fs.writeFileSync(manifestPath, manifestData, { encoding: "utf8" });
