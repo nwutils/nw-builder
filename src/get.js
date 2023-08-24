363380ddb8f72ddc0fb92c950aa5484ca9e87a47
Ayushman Chhabra <ayushmanchhabra@users.noreply.github.com>
2023-08-24 11:12:43 -0400

    fix(get): remove symbolic links workaround

--- src/get.js
+++ src/get.js
@@ -10,7 +10,6 @@ import compressing from "compressing";
 import { log } from "./log.js";
 import { PLATFORM_KV, ARCH_KV } from "./util.js";
 import { replaceFfmpeg } from "./util/ffmpeg.js";
-import child_process from "child_process";
 
 /**
  * _Note: This an internal function which is not called directly. Please see example usage below._
@@ -163,22 +162,6 @@ export async function get({
               compressing.tgz
                 .uncompress(out, ffmpeg ? nwDir : cacheDir)
                 .then(() => resolve());
-            } else if (platform === "osx") {
-              //TODO: compressing package does not restore symlinks on some macOS (eg: circleCI)
-              const exec = function (cmd) {
-                log.debug(cmd);
-                const result = child_process.spawnSync(cmd, {
-                  shell: true,
-                  stdio: "inherit",
-                });
-                if (result.status !== 0) {
-                  log.debug(`Command failed with status ${result.status}`);
-                  if (result.error) console.log(result.error);
-                  process.exit(1);
-                }
-                return resolve();
-              };
-              exec(`unzip -o "${out}" -d "${ffmpeg ? nwDir : cacheDir}"`);
             } else {
               compressing.zip
                 .uncompress(out, ffmpeg ? nwDir : cacheDir)
