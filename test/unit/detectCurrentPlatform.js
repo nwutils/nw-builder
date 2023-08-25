9d03df9a9805ffb9c24effca150b4daa6d0b85d3
Ayushman Chhabra <ayushmanchhabra@users.noreply.github.com>
2023-08-25 14:13:19 -0400

    test: migrate from Jest to Node Test Runner

---
+++ test/unit/detectCurrentPlatform.js
@@ -0,0 +1,68 @@
+import { strictEqual } from "node:assert";
+import {
+  describe,
+  it,
+} from "node:test";
+
+import detectCurrentPlatform from "../../src/utilities/detectCurrentPlatform.js";
+import Platform from "../../src/constants/Platform.js";
+
+describe("detectCurrentPlatform", () => {
+
+  it("for OSX 32 platform", () => {
+    const process = {
+      platform: "darwin",
+      arch: "x32",
+    };
+
+    strictEqual(detectCurrentPlatform(process), Platform.OSX_32);
+  });
+
+  it("for OSX 64 platform", () => {
+    const process = {
+      platform: "darwin",
+      arch: "x64",
+    };
+
+    strictEqual(detectCurrentPlatform(process), Platform.OSX_64);
+  });
+
+  it("for Linux 32 platform", () => {
+    const process = {
+      platform: "linux",
+      arch: "x32",
+    };
+
+    strictEqual(detectCurrentPlatform(process), Platform.NIX_32);
+  });
+
+  it("for Linux 64 platform", () => {
+    const process = {
+      platform: "linux",
+      arch: "x64",
+    };
+
+    strictEqual(detectCurrentPlatform(process), Platform.NIX_64);
+  });
+
+  it("for Windows 32 platform", () => {
+    const process = {
+      platform: "win32",
+      arch: "x32",
+    };
+
+    strictEqual(detectCurrentPlatform(process), Platform.WIN_32);
+  });
+
+  it("for Windows 64 platform", () => {
+    const process = {
+      platform: "win32",
+      arch: "x64",
+    };
+
+    strictEqual(detectCurrentPlatform(process), Platform.WIN_64);
+  });
+
+});
+
+export { };
