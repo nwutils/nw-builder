7864cd20790e78eb2ab166cd2dfcfb4bea206be2
Ayushman Chhabra <ayushmanchhabra@users.noreply.github.com>
2023-08-25 12:23:43 -0400

    test: disable Selenium and enable Tape

--- test/demo.cjs
+++ test/demo.cjs
@@ -2,8 +2,12 @@ const NwBuilder = require("../lib/index.cjs");
 
 const nw = new NwBuilder({
   files: ["demo/*"],
-  platforms: ["linux64", "osx64", "win64"],
+  platforms: ["win64"],
   version: "0.79.1",
+  flavor: "sdk",
+  // These dirs are used by Selenium too
+  cacheDir: "../cache",
+  buildDir: "../build",
 });
 
-nw.build();
+nw.run();
