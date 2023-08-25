bbed867067c76debc133229e937644b4a3a65972
Ayushman Chhabra <ayushmanchhabra@users.noreply.github.com>
2023-08-25 14:55:49 -0400

    test: use same cache used by Seelnium

--- test/demo.cjs
+++ test/demo.cjs
@@ -2,8 +2,11 @@ const NwBuilder = require("../lib/index.cjs");
 
 const nw = new NwBuilder({
   files: ["demo/*"],
-  platforms: ["linux64", "osx64", "win64"],
+  platforms: ["win64"],
   version: "0.79.1",
+  flavor: "sdk",
+  cacheDir: "../cache",
+  buildDir: "../build",
 });
 
 nw.build();
