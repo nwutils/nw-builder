7f0c713bf7d35afa7bb2d0481568047d4466ba43
Ayushman Chhabra <ayushmanchhabra@users.noreply.github.com>
2023-08-28 00:24:55 -0400

    test: update demo

--- test/demo.cjs
+++ test/demo.cjs
@@ -1,20 +1,12 @@
 const NwBuilder = require("../lib/index.cjs");
 
-// const nw = new NwBuilder({
-//   files: ["demo/*"],
-//   platforms: ["win64"],
-//   version: "0.79.1",
-//   flavor: "sdk",
-//   cacheDir: "../cache",
-//   buildDir: "../build",
-// });
-
-// nw.run();
-
 const nw = new NwBuilder({
   files: ["demo/*"],
-  platforms: undefined,
-  buildType: "timestamped"
+  platforms: ["win64"],
+  version: "0.79.1",
+  flavor: "sdk",
+  cacheDir: "../cache",
+  buildDir: "../build",
 });
 
-nw.init()
+nw.run();
