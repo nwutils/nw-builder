d8ad2deb82efcabaaa5fd14619a580815cf29015
Zayin Krige <zkrige@gmail.com>
2023-09-04 08:14:58 +0200

    update test

--- test/e2e/test.js
+++ test/e2e/test.js
@@ -8,7 +8,6 @@ describe("test fetch nwjs and ffmpeg", async () => {
   let nwOptions = {
     srcDir: "test/fixture/app",
     mode: "get",
-    version: "0.78.1",
     flavor: "sdk",
     platform: getPlatform(platform),
     arch: getArch(arch),
