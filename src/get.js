066824a5de6d9c9ad655d13bc261029d427c56ac
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2023-10-06 18:22:58 -0400

    fix: decompression

--- src/get.js
+++ src/get.js
@@ -206,9 +206,7 @@ async function get_nwjs({
                 try {
                   for await (const entry of zip) {
                     if (entry.filename.endsWith("/")) {
-                      await mkdir(`${cacheDir}/${entry.filename}`, {
-                        recursive: true,
-                      });
+                      await mkdir(`${cacheDir}/${entry.filename}`);
                     } else {
                       const readStream = await entry.openReadStream();
                       const writeStream = createWriteStream(
