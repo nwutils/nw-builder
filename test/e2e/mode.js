8adcd53aa0e5ed49760ad3ee9c5abe8c66848cbc
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2023-10-16 12:43:28 -0400

    test: revert

--- test/e2e/mode.js
+++ test/e2e/mode.js
@@ -1,33 +1,34 @@
 import { equal } from "node:assert";
 import { arch, platform } from "node:process";
-import { basename, resolve } from "node:path";
+import { resolve } from "node:path";
 import { describe, it } from "node:test";
 
 import nwbuild from "nw-builder";
 import { By } from "selenium-webdriver";
 import chrome from "selenium-webdriver/chrome.js";
 
-import { ARCH_KV, PLATFORM_KV } from "../../src/util.js";
+import { getArch } from "../../src/util/arch.js";
+import { getPlatform } from "../../src/util/platform.js";
 
 const { Driver, ServiceBuilder, Options } = chrome;
 
 describe("test modes", async () => {
   let driver = undefined;
 
-  let nwOptions = {};
+  let nwOptions = {
+    srcDir: "test/fixture/app",
+    mode: "build",
+    version: "0.80.0",
+    flavor: "sdk",
+    platform: getPlatform(platform),
+    arch: getArch(arch),
+    outDir: "test/fixture/out",
+    cacheDir: "test/fixture/cache",
+    glob: false,
+  };
 
-  it("runs application", async () => {
-    await nwbuild({
-      srcDir: "test/fixture/app",
-      mode: "build",
-      version: "0.80.0",
-      flavor: "sdk",
-      platform: PLATFORM_KV[platform],
-      arch: ARCH_KV[arch],
-      outDir: "test/fixture/out",
-      cacheDir: "test/fixture/cache",
-      glob: false,
-    });
+  it("should run", async () => {
+    await nwbuild({ ...nwOptions });
 
     const options = new Options();
     const args = [
@@ -49,35 +50,5 @@ describe("test modes", async () => {
     driver = Driver.createSession(options, service);
     const text = await driver.findElement(By.id("test")).getText();
     equal(text, "Hello, World!");
-  });
-
-  it("renames MacOS Helper Apps", async () => {
-    if (PLATFORM_KV[platform] === "darwin") {
-      await nwbuild({
-        srcDir: "test/fixture/app",
-        mode: "build",
-        version: "0.80.0",
-        platform: PLATFORM_KV[platform],
-        arch: ARCH_KV[arch],
-        outDir: "test/fixture/out",
-        cacheDir: "test/fixture/cache",
-        glob: false,
-        app: {
-          name: "demo",
-          icon: "app/icon.icns",
-          CFBundleIdentifier: "io.nwutils.demo",
-          CFBundleName: "demo",
-          CFBundleDisplayName: "demo",
-          CFBundleVersion: "0.0.0",
-          CFBundleShortVersionString: "0.0.0",
-          NSHumanReadableCopyright: "Copyright (c) 2023 NW.js Utilities",
-        },
-      });
-
-      const appName = basename(
-        resolve(nwOptions.outDir, `${nwOptions.app.name}.app`),
-      );
-      equal(appName, "demo.app");
-    }
   });
 });
