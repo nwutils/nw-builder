a70025666613317320b9d9b9cb1c10891db7899b
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2023-11-30 23:07:31 -0500

    fix: format

--- .github/fswatch.config.js
+++ .github/fswatch.config.js
@@ -1,16 +1,15 @@
 import child_process from "node:child_process";
 import fsm from "node:fs/promises";
 
-(async () => {
-    try {
-        const watcher = fsm.watch("src", { recursive: true });
-        for await (const event of watcher) {
-            if (event) {
-                child_process.exec("npm run doc:bld");
-            }
-        }
-
-    } catch (e) {
-        console.error(e);
+async () => {
+  try {
+    const watcher = fsm.watch("src", { recursive: true });
+    for await (const event of watcher) {
+      if (event) {
+        child_process.exec("npm run doc:bld");
+      }
     }
-});
+  } catch (e) {
+    console.error(e);
+  }
+};
