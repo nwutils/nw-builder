a84f3c3e31708ba31ef0079da42837529b9abefa
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2023-11-30 23:51:51 -0500

    fix: docs file watcher

--- .github/fswatch.config.js
+++ .github/fswatch.config.js
@@ -1,15 +1,13 @@
 import child_process from "node:child_process";
 import fsm from "node:fs/promises";
 
-async () => {
-  try {
-    const watcher = fsm.watch("src", { recursive: true });
-    for await (const event of watcher) {
-      if (event) {
-        child_process.exec("npm run doc:bld");
-      }
+try {
+  const watcher = await fsm.watch("src", { recursive: true });
+  for await (const event of watcher) {
+    if (event) {
+      child_process.exec("npm run doc:bld");
     }
-  } catch (e) {
-    console.error(e);
   }
-};
+} catch (e) {
+  console.error(e);
+}
