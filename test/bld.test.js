bcf5c478ca25a18e0d8f846c1dd079932d017234
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2023-12-26 13:36:22 +0530

    test: simplify repro

--- test/bld.test.js
+++ test/bld.test.js
@@ -1,16 +1,8 @@
-import { equal } from "node:assert";
-import { arch, platform } from "node:process";
-import { resolve } from "node:path";
 import { describe, it } from "node:test";
 
 import nwbuild from "../src/index.js";
-import { By } from "selenium-webdriver";
-import chrome from "selenium-webdriver/chrome.js";
 
-const { Driver, ServiceBuilder, Options } = chrome;
-
-describe("bld", async () => {
-  let driver = undefined;
+describe("bld", () => {
 
   let nwOptions = {
     srcDir: "test/fixture/app",
@@ -23,31 +15,11 @@ describe("bld", async () => {
     cacheDir: "test/fixture/cache",
     glob: false,
     app: {
-        icon: "icon.ico"
+      icon: "icon.ico"
     }
   };
 
-  it("should build and run", async () => {
+  it("builds Windows application", async () => {
     await nwbuild({ ...nwOptions });
-
-    const options = new Options();
-    const args = [
-      `--nwapp=${resolve("test", "fixture", "out", "app", "package.nw")}`,
-      "--headless=new",
-    ];
-    options.addArguments(args);
-
-    const chromedriverPath = resolve(
-      nwOptions.cacheDir,
-      `nwjs${nwOptions.flavor === "sdk" ? "-sdk" : ""}-v${nwOptions.version}-${nwOptions.platform
-      }-${nwOptions.arch}`,
-      `chromedriver${nwOptions.platform === "win" ? ".exe" : ""}`,
-    );
-
-    const service = new ServiceBuilder(chromedriverPath).build();
-
-    driver = Driver.createSession(options, service);
-    const text = await driver.findElement(By.id("test")).getText();
-    equal(text, "Hello, World!");
   });
 });
