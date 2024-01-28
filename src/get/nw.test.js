3fc404e0c2786cebace9888ab12ce6c36f4900e0
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-01-28 12:47:58 -0500

    chore(get): factor out nw download logic

---
+++ src/get/nw.test.js
@@ -0,0 +1,20 @@
+import { describe, it } from "vitest";
+
+import util from "../util.js";
+
+import nw from "./nw.js";
+
+describe("get/nw", function () {
+
+  it("downloades a NW.js Linux tarball", async function () {
+    const nwFile = await nw(
+      "https://nwjs.io/versions",
+      "0.83.0",
+      "sdk",
+      "linux",
+      "x64",
+      "./test/fixture"
+    );
+    expect(util.fileExists(nwFile)).resolves.toBe(true);
+  }, Infinity);
+});
