6b0397c23d0f817b9d2e3fe7ee9940d7f95cdb6e
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-01-28 13:03:51 -0500

    chore(test): simplfy test per platform

--- src/get/nw.test.js
+++ src/get/nw.test.js
@@ -1,4 +1,7 @@
-import { describe, expect, it } from "vitest";
+import fs from "node:fs";
+import process from "node:process";
+
+import { afterEach, describe, expect, it } from "vitest";
 
 import util from "../util.js";
 
@@ -6,25 +9,19 @@ import nw from "./nw.js";
 
 describe("get/nw", function () {
 
-  it("downloades a NW.js Linux tarball", async function () {
-    const nwFile = await nw(
-      "https://dl.nwjs.io",
-      "0.83.0",
-      "sdk",
-      "linux",
-      "x64",
-      "./test/fixture"
-    );
-    expect(util.fileExists(nwFile)).resolves.toBe(true);
-  }, Infinity);
+  let nwFile = '';
 
-  it("downloades a NW.js MacOS zip", async function () {
-    const nwFile = await nw(
+  afterEach(function () {
+    fs.promises.rm(nwFile, { recursive: true, force: true });
+  });
+
+  it("downloades a NW.js Linux tarball or Windows/MacOS zip", async function () {
+    nwFile = await nw(
       "https://dl.nwjs.io",
       "0.83.0",
       "sdk",
-      "osx",
-      "x64",
+      util.PLATFORM_KV[process.platform],
+      util.ARCH_KV[process.arch],
       "./test/fixture"
     );
     expect(util.fileExists(nwFile)).resolves.toBe(true);
