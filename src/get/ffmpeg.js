ed51fa2f7e03746cf34da6e9444029cdf22cd7a8
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-01-28 15:05:59 -0500

    chore(get): update code comments

--- src/get/ffmpeg.js
+++ src/get/ffmpeg.js
@@ -3,14 +3,14 @@ import path from "node:path";
 import request from "./request.js";
 
 /**
- * Download community FFpeg binary from `https://github.com/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt`.
+ * Download community FFmpeg binary from `https://github.com/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt`.
  *
  * @param  {string}          downloadUrl  - Download server
  * @param  {string}          version      - Runtime version
  * @param  {string}          platform     - NW supported platform
  * @param  {string}          arch         - NW supported architecture
  * @param  {string}          cacheDir     - Directory to store NW binaries
- * @return {Promise<string>}              - path of compressed file which contains NW.js binaries.
+ * @return {Promise<string>}              - path of compressed file which containscommunity FFmpeg binary.
  */
 export default async function ffmpeg(downloadUrl, version, platform, arch, cacheDir) {
 
