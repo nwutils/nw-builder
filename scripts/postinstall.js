8e8dae50c48d386ed0a31673e82627caa4831287
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2024-06-21 21:50:07 -0400

    Apply suggestions from code review

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -7,6 +7,14 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const baseVoltaPath = resolve(join(__dirname, '..', 'node_modules', 'base-volta-off-of-nwjs', 'index.js'));
 
+/**
+ * When developing locally, we want to make sure the Node.js version
+ * documented in the `volta` object in the `package.json` matches
+ * whatever version of NW.js is installed, for manual testing purposes.
+ * However, for those installing this library as a devDependency, they
+ * will not have `base-volta-off-of-nwjs` installed, and nor should
+ * they. So we skip this check for them.
+ */
 if (existsSync(baseVoltaPath)) {
   execSync('node ' + baseVoltaPath);
 }
