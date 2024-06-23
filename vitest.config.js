122f927c3ee08e6aadd4ab1d263a4baffb2db537
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-06-23 22:15:55 +0530

    chore(test): remove text reporter

--- vitest.config.js
+++ vitest.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json-summary'],
+      reporter: ['json-summary'],
       reportOnFailure: true,
     }
   }
