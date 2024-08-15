7d6acfad97f769dec5f7f9a794ed53c352d78271
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-08-15 12:24:11 +0530

    chore: loosen function constraints

--- src/get/verify.js
+++ src/get/verify.js
@@ -6,31 +6,23 @@ import request from './request.js';
 
 import util from '../util.js';
 
-console.log(await verify('0.90.0', 'sdk', 'linux', 'x64', './node_modules/nw'));
+console.log(await verify('https://dl.nwjs.io/v0.90.0/SHASUMS256.txt', './node_modules/nw/shasum/0.90.0.txt', './node_modules/nw/nwjs-sdk-v0.90.0-linux-x64.tar.gz'));
 
-export default async function verify(version, flavor, platform, arch, cacheDir) {
-  const shaUrl = `https://dl.nwjs.io/v${version}/SHASUMS256.txt`;
-  const shaOut = path.resolve(cacheDir, 'shasum', `${version}.txt`);
+export default async function verify(shaUrl, shaOut, filePath) {
   const shaOutExists = await util.fileExists(shaOut);
-  let nwFilePath = path.resolve(
-    cacheDir,
-    `nwjs${flavor === "sdk" ? "-sdk" : ""}-v${version}-${platform}-${arch}.${platform === "linux" ? "tar.gz" : "zip"
-    }`,
-  );
 
-  /* Create directory if does not exist. */
   if (shaOutExists === false) {
+    /* Create directory if does not exist. */
     await fs.promises.mkdir(path.dirname(shaOut), { recursive: true });
-  }
 
-  /* Download SHASUM text file. */
-  await request(shaUrl, shaOut);
+    /* Download SHASUM text file. */
+    await request(shaUrl, shaOut);
+  }
 
   const shasum = await fs.promises.readFile(shaOut, { encoding: 'utf-8' });
   const shasumLines = shasum.split('\n');
   for (const line of shasumLines) {
     const [sha, file] = line.split('  ');
-    const filePath = path.resolve(cacheDir, nwFilePath);
     if (file === path.basename(filePath)) {
       const fileBuffer = await fs.promises.readFile(filePath);
       const hash = crypto.createHash('sha256');
