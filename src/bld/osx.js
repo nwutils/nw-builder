1ad3870cfadf82a60c22fcf6f22b69e7d7f4af0e
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-08-19 14:36:36 +0530

    chore(bld): refactor osx config

--- src/bld/osx.js
+++ src/bld/osx.js
@@ -22,48 +22,57 @@ export default async function setOsxConfig({ outDir, app }) {
       );
     }
 
-    const infoPlistPath = path.resolve(outApp, "Contents", "Info.plist");
-    const infoPlistJson = plist.parse(await fs.promises.readFile(infoPlistPath, "utf-8"));
-
-    const infoPlistStringsPath = path.resolve(
+    const ContentsInfoPlistPath = path.resolve(
+      outApp,
+      "Contents",
+      "Info.plist"
+    );
+    const ContentsResourcesEnLprojInfoPlistStringsPath = path.resolve(
       outApp,
       "Contents",
       "Resources",
       "en.lproj",
       "InfoPlist.strings",
     );
-    const infoPlistStringsData = await fs.promises.readFile(
-      infoPlistStringsPath,
-      "utf-8",
+
+    const ContentsInfoPlistJson = plist.parse(
+      await fs.promises.readFile(
+        ContentsInfoPlistPath,
+        "utf-8"
+      )
     );
 
-    let infoPlistStringsDataArray = infoPlistStringsData.split("\n");
+    ContentsInfoPlistJson.LSApplicationCategoryType = app.LSApplicationCategoryType;
+    ContentsInfoPlistJson.CFBundleIdentifier = app.CFBundleIdentifier;
+    ContentsInfoPlistJson.CFBundleName = app.CFBundleName;
+    ContentsInfoPlistJson.CFBundleDisplayName = app.CFBundleDisplayName;
+    ContentsInfoPlistJson.CFBundleSpokenName = app.CFBundleSpokenName;
+    ContentsInfoPlistJson.CFBundleVersion = app.CFBundleVersion;
+    ContentsInfoPlistJson.CFBundleShortVersionString = app.CFBundleShortVersionString;
 
-    infoPlistStringsDataArray.forEach((line, idx, arr) => {
+    Object.keys(ContentsInfoPlistJson).forEach((option) => {
+      if (ContentsInfoPlistJson[option] === undefined) {
+        delete ContentsInfoPlistJson[option];
+      }
+    });
+
+    const ContentsResourcesEnLprojInfoPlistStringsArray = (await fs.promises.readFile(
+      ContentsResourcesEnLprojInfoPlistStringsPath,
+      "utf-8",
+    )).split('\n');
+    ContentsResourcesEnLprojInfoPlistStringsArray.forEach((line, idx, arr) => {
       if (line.includes("NSHumanReadableCopyright")) {
         arr[idx] =
           `NSHumanReadableCopyright = "${app.NSHumanReadableCopyright}";`;
       }
     });
 
-    infoPlistJson.LSApplicationCategoryType = app.LSApplicationCategoryType;
-    infoPlistJson.CFBundleIdentifier = app.CFBundleIdentifier;
-    infoPlistJson.CFBundleName = app.CFBundleName;
-    infoPlistJson.CFBundleDisplayName = app.CFBundleDisplayName;
-    infoPlistJson.CFBundleSpokenName = app.CFBundleSpokenName;
-    infoPlistJson.CFBundleVersion = app.CFBundleVersion;
-    infoPlistJson.CFBundleShortVersionString = app.CFBundleShortVersionString;
-
-    Object.keys(infoPlistJson).forEach((option) => {
-      if (infoPlistJson[option] === undefined) {
-        delete infoPlistJson[option];
-      }
-    });
-
-    await fs.promises.writeFile(infoPlistPath, plist.build(infoPlistJson));
     await fs.promises.writeFile(
-      infoPlistStringsPath,
-      infoPlistStringsDataArray.toString().replace(/,/g, "\n"),
+      ContentsInfoPlistPath,
+      plist.build(ContentsInfoPlistJson));
+    await fs.promises.writeFile(
+      ContentsResourcesEnLprojInfoPlistStringsPath,
+      ContentsResourcesEnLprojInfoPlistStringsArray.toString().replace(/,/g, "\n"),
     );
   } catch (error) {
     console.error(error);
