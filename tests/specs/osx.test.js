e44951c2a65e1409f580f6ea8fbca46642bb2738
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-08-19 18:57:03 +0530

    chore(test): prepare osx testing

--- tests/specs/osx.test.js
+++ tests/specs/osx.test.js
@@ -2,13 +2,24 @@ import fs from "node:fs";
 import path from "node:path";
 import process from "node:process";
 
-import { describe, expect, it } from "vitest";
+import { beforeAll, describe, expect, it } from "vitest";
 
 import setOsxConfig from "../../src/bld/osx.js";
 
 describe("bld/setOsxConfig", async function () {
 
+  beforeAll("", async function () {
+    // copy nwjs exe to outDir
+    // call setOsxConfig function
+  });
+
   it("", async function () {
+    // verify if apps are renamed properly
+    expect(1+1).toEqual(2);
+  });
+
+  it("", async function () {
+    // verify if bundle's plist values are renamed properly
     expect(1+1).toEqual(2);
   });
 });
