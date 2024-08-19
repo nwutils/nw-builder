d19cddf13c38fd87b00fcc8c949085aa19f42da5
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-08-19 18:51:25 +0530

    chore(test): setup osx testing

---
+++ tests/specs/osx.test.js
@@ -0,0 +1,14 @@
+import fs from "node:fs";
+import path from "node:path";
+import process from "node:process";
+
+import { describe, expect, it } from "vitest";
+
+import setOsxConfig from "../../src/bld/osx.js";
+
+describe("bld/setOsxConfig", async function () {
+
+  it("", async function () {
+    expect(1+1).toEqual(2);
+  });
+});
