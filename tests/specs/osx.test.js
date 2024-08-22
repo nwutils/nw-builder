e4ea4748af79c9de8ef256346208b32dfd0239e4
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-08-22 23:27:55 +0530

    chore(test): coverage rename of nwjs.app

--- tests/specs/osx.test.js
+++ tests/specs/osx.test.js
@@ -2,22 +2,54 @@ import fs from "node:fs";
 import path from "node:path";
 import process from "node:process";
 
-import { beforeAll, describe, expect, it } from "vitest";
+import * as nw from 'nw';
+import plist from 'plist';
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
 import setOsxConfig from "../../src/bld/osx.js";
+import util from "../../src/util.js";
 
-describe("bld/setOsxConfig", async function () {
+describe
+// .runIf(process.platform === 'darwin')
+("bld/setOsxConfig", async function () {
 
-  beforeAll("", async function () {
-    // copy nwjs exe to outDir
-    // call setOsxConfig function
+  const outDir = './tests/fixtures/macos';
+
+  beforeAll(async function () {
+    /* Copy the cached NW.js into a specific `outDir`. */
+    await fs.promises.cp(nw.findpath('all', { flavor: 'sdk' }), outDir, { recursive: true, force: true });
+
+    /* Rename relevant bundles' plists and executables. */
+    await setOsxConfig({
+      app: {
+        name: 'nwapp',
+        LSApplicationCategoryType: 'public.app-category.utilities',
+        CFBundleIdentifier: 'io.nwutils.demo',
+        CFBundleName: 'Demo',
+        CFBundleDisplayName: 'Demo',
+        CFBundleSpokenName: 'Demo',
+        CFBundleVersion: '0.0.0',
+        CFBundleShortVersionString: '0.0.0',
+        NSHumanReadableCopyright: 'Copyright (c) 2024 NW.js Utilities'
+      },
+      outDir: outDir,
+      releaseInfo: { components: {
+        chromium: "127.0.6533.73",
+      }},
+    });
   });
 
-  it("", async function () {
-    // verify if apps are renamed properly
+  it("renames the apps and executables correctly", async function () {
+    const appPath = path.join(outDir, 'nwapp.app');
+    const appPathExists = await util.fileExists(appPath);
+    expect(appPathExists).toEqual(true);
   });
 
   it("", async function () {
     // verify if bundle's plist values are renamed properly
+  });
+
+  afterAll(async function () {
+    await fs.promises.rm('./tests/fixtures/macos', { recursive: true, force: true });
   });
 });
