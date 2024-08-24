0b4187c07c3acd677c33f25e0356b075b255b338
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-08-24 20:27:49 +0530

    chore: lint whole codebase

--- src/cli.js
+++ src/cli.js
@@ -1,95 +1,95 @@
 #!/usr/bin/env node
 
-import process from "node:process";
+import process from 'node:process';
 
-import yargs from "yargs/yargs";
-import * as yargs_helpers from "yargs/helpers";
+import yargs from 'yargs/yargs';
+import * as yargs_helpers from 'yargs/helpers';
 
-import nwbuild from "./index.js";
+import nwbuild from './index.js';
 
 const cli = yargs(yargs_helpers.hideBin(process.argv))
   .version(false)
-  .command("[srcDir] [options]")
-  .option("mode", {
-    type: "string",
-    description: "`get`, `run` or `build` application",
-    choices: ["get", "run", "build"]
+  .command('[srcDir] [options]')
+  .option('mode', {
+    type: 'string',
+    description: '`get`, `run` or `build` application',
+    choices: ['get', 'run', 'build']
   })
-  .option("version", {
-    type: "string",
-    description: "NW.js version",
+  .option('version', {
+    type: 'string',
+    description: 'NW.js version',
   })
-  .option("flavor", {
-    type: "string",
-    description: "NW.js build flavor",
-    choices: ["normal", "sdk"]
+  .option('flavor', {
+    type: 'string',
+    description: 'NW.js build flavor',
+    choices: ['normal', 'sdk']
   })
-  .option("platform", {
-    type: "string",
-    description: "NW.js supported platform",
-    choices: ["linux", "osx", "win"]
+  .option('platform', {
+    type: 'string',
+    description: 'NW.js supported platform',
+    choices: ['linux', 'osx', 'win']
   })
-  .option("arch", {
-    type: "string",
-    description: "NW.js supported architecture",
-    choices: ["ia32", "x64", "arm64"]
+  .option('arch', {
+    type: 'string',
+    description: 'NW.js supported architecture',
+    choices: ['ia32', 'x64', 'arm64']
   })
-  .option("downloadUrl", {
-    type: "string",
-    description: "NW.js download server",
+  .option('downloadUrl', {
+    type: 'string',
+    description: 'NW.js download server',
   })
-  .option("manifestUrl", {
-    type: "string",
-    description: "NW.js version info",
+  .option('manifestUrl', {
+    type: 'string',
+    description: 'NW.js version info',
   })
-  .option("cacheDir", {
-    type: "string",
-    description: "Cache NW.js binaries",
+  .option('cacheDir', {
+    type: 'string',
+    description: 'Cache NW.js binaries',
   })
-  .option("outDir", {
-    type: "string",
-    description: "NW.js build artifacts",
+  .option('outDir', {
+    type: 'string',
+    description: 'NW.js build artifacts',
   })
-  .option("app", {
-    type: "object",
-    description: "Platform specific app metadata. Refer to docs for more info",
+  .option('app', {
+    type: 'object',
+    description: 'Platform specific app metadata. Refer to docs for more info',
   })
-  .option("cache", {
-    type: "boolean",
-    description: "Flag to enable/disable caching",
+  .option('cache', {
+    type: 'boolean',
+    description: 'Flag to enable/disable caching',
   })
-  .option("ffmpeg", {
-    type: "boolean",
-    description: "Flag to enable/disable downloading community ffmpeg",
+  .option('ffmpeg', {
+    type: 'boolean',
+    description: 'Flag to enable/disable downloading community ffmpeg',
   })
-  .option("glob", {
-    type: "boolean",
-    description: "Flag to enable/disable globbing",
+  .option('glob', {
+    type: 'boolean',
+    description: 'Flag to enable/disable globbing',
   })
-  .option("logLevel", {
-    type: "string",
-    description: "Specify log level",
-    choices: ["error", "warn", "info", "debug"]
+  .option('logLevel', {
+    type: 'string',
+    description: 'Specify log level',
+    choices: ['error', 'warn', 'info', 'debug']
   })
-  .option("zip", {
-    type: "string",
-    description: "Flag to enable/disable compression",
-    choices: ["zip", "tar", "tgz"]
+  .option('zip', {
+    type: 'string',
+    description: 'Flag to enable/disable compression',
+    choices: ['zip', 'tar', 'tgz']
   })
-  .option("managedManifest", {
-    type: "string",
-    description: "Managed manifest mode",
+  .option('managedManifest', {
+    type: 'string',
+    description: 'Managed manifest mode',
   })
-  .option("nodeAddon", {
-    type: "string",
-    description: "Download NW.js Node headers",
-    choices: [false, "gyp"]
+  .option('nodeAddon', {
+    type: 'string',
+    description: 'Download NW.js Node headers',
+    choices: [false, 'gyp']
   })
   .strictOptions()
   .parse();
 
 nwbuild({
   ...cli,
-  srcDir: cli._.join(" "),
+  srcDir: cli._.join(' '),
   cli: true,
 });
--- src/util.js
+++ src/util.js
@@ -1,37 +1,36 @@
-import console from "node:console";
-import fs from "node:fs";
-import https from "node:https";
-import path from "node:path";
-import process from "node:process";
+import console from 'node:console';
+import fs from 'node:fs';
+import https from 'node:https';
+import path from 'node:path';
+import process from 'node:process';
 
-import * as GlobModule from "glob";
-import semver from "semver";
+import * as GlobModule from 'glob';
+import semver from 'semver';
 
 /**
  * Get manifest (array of NW release metadata) from URL.
- *
  * @param  {string}                      manifestUrl  Url to manifest
- * @return {Promise<object | undefined>}              - Manifest object
+ * @returns {Promise<object | undefined>}              - Manifest object
  */
 function getManifest(manifestUrl) {
   let chunks = undefined;
 
   return new Promise((resolve) => {
     const req = https.get(manifestUrl, (response) => {
-      response.on("data", (chunk) => {
+      response.on('data', (chunk) => {
         chunks += chunk;
       });
 
-      response.on("error", (e) => {
+      response.on('error', (e) => {
         console.error(e);
         resolve(undefined);
       });
 
-      response.on("end", () => {
+      response.on('end', () => {
         resolve(chunks);
       });
     });
-    req.on("error", (e) => {
+    req.on('error', (e) => {
       console.error(e);
       resolve(undefined);
     });
@@ -40,13 +39,12 @@ function getManifest(manifestUrl) {
 
 /**
  * Get version specific release metadata.
- *
  * @param  {string} version      NW version
  * @param  {string} platform     NW platform
  * @param  {string} arch         NW architecture
  * @param  {string} cacheDir     Directory to store NW binaries
  * @param  {string} manifestUrl  Url to manifest
- * @return {object}              Version specific release info
+ * @returns {object}              Version specific release info
  */
 async function getReleaseInfo(
   version,
@@ -57,10 +55,10 @@ async function getReleaseInfo(
 ) {
   let releaseData = undefined;
   let manifestPath = undefined;
-  if (platform === "osx" && arch === "arm64") {
-    manifestPath = path.resolve(cacheDir, "manifest.mac.arm.json");
+  if (platform === 'osx' && arch === 'arm64') {
+    manifestPath = path.resolve(cacheDir, 'manifest.mac.arm.json');
   } else {
-    manifestPath = path.resolve(cacheDir, "manifest.json");
+    manifestPath = path.resolve(cacheDir, 'manifest.json');
   }
 
   try {
@@ -70,7 +68,7 @@ async function getReleaseInfo(
     }
 
     let manifest = JSON.parse(await fs.promises.readFile(manifestPath));
-    if (version === "latest" || version === "stable" || version === "lts") {
+    if (version === 'latest' || version === 'stable' || version === 'lts') {
       // Remove leading "v" from version string
       version = manifest[version].slice(1);
     }
@@ -78,42 +76,40 @@ async function getReleaseInfo(
     releaseData = manifest.versions.find(
       (release) => release.version === `v${version}`,
     );
-  } catch (e) {
+  } catch {
     console.error(
-      "The version manifest does not exist/was not downloaded. Please try again in some time.",
+      'The version manifest does not exist/was not downloaded. Please try again in some time.',
     );
   }
   return releaseData;
 }
 
 const PLATFORM_KV = {
-  darwin: "osx",
-  linux: "linux",
-  win32: "win",
+  darwin: 'osx',
+  linux: 'linux',
+  win32: 'win',
 };
 
 const ARCH_KV = {
-  x64: "x64",
-  ia32: "ia32",
-  arm64: "arm64",
+  x64: 'x64',
+  ia32: 'ia32',
+  arm64: 'arm64',
 };
 
 const EXE_NAME = {
-  win: "nw.exe",
-  osx: "nwjs.app/Contents/MacOS/nwjs",
-  linux: "nw",
+  win: 'nw.exe',
+  osx: 'nwjs.app/Contents/MacOS/nwjs',
+  linux: 'nw',
 };
 
 /**
  * Glob files.
- *
  * @async
  * @function
- *
  * @param  {object}            options         - glob file options
  * @param  {string | string[]} options.srcDir  - app src dir
  * @param  {boolean}           options.glob    - glob flag
- * @return {Promise<string[]>}                 - Returns array of file paths
+ * @returns {Promise<string[]>}                 - Returns array of file paths
  */
 async function globFiles({
   srcDir,
@@ -122,7 +118,7 @@ async function globFiles({
   let files;
   if (glob) {
     files = [];
-    const patterns = srcDir.split(" ");
+    const patterns = srcDir.split(' ');
     for (const pattern of patterns) {
       let filePath = await GlobModule.glob(pattern);
       files.push(...filePath);
@@ -135,13 +131,12 @@ async function globFiles({
 
 /**
  * Get Node manifest.
- *
  * @async
  * @function
  * @param  {object}             options         - node manifest options
  * @param  {string | string []} options.srcDir  - src dir
  * @param  {boolean}            options.glob    - glob flag
- * @return {object}                             - Node manifest
+ * @returns {object}                             - Node manifest
  */
 async function getNodeManifest({
   srcDir, glob
@@ -151,16 +146,16 @@ async function getNodeManifest({
   if (glob) {
     files = await globFiles({ srcDir, glob });
     for (const file of files) {
-      if (path.basename(file) === "package.json" && manifest === undefined) {
+      if (path.basename(file) === 'package.json' && manifest === undefined) {
         manifest = JSON.parse(await fs.promises.readFile(file));
       }
     }
   } else {
-    manifest = JSON.parse(await fs.promises.readFile(path.resolve(srcDir, "package.json")));
+    manifest = JSON.parse(await fs.promises.readFile(path.resolve(srcDir, 'package.json')));
   }
 
   if (manifest === undefined) {
-    throw new Error("package.json not found in srcDir file glob patterns.");
+    throw new Error('package.json not found in srcDir file glob patterns.');
   }
 
   return manifest;
@@ -168,39 +163,38 @@ async function getNodeManifest({
 
 /**
  * Parse options.
- *
  * @param  {import("../../index.js").Options} options  Options
  * @param  {object}                           pkg      Package.json as JSON
- * @return {Promise<object>}                           Options
+ * @returns {Promise<object>}                           Options
  */
 export const parse = async (options, pkg) => {
   options = options ?? {};
-  options.mode = options.mode ?? "build";
+  options.mode = options.mode ?? 'build';
 
-  options.version = options.version ?? "latest";
-  options.flavor = options.flavor ?? "normal";
+  options.version = options.version ?? 'latest';
+  options.flavor = options.flavor ?? 'normal';
   options.platform = options.platform ?? PLATFORM_KV[process.platform];
   options.arch = options.arch ?? ARCH_KV[process.arch];
-  options.downloadUrl = options.downloadUrl ?? "https://dl.nwjs.io";
-  options.manifestUrl = options.manifestUrl ?? "https://nwjs.io/versions";
-  options.cacheDir = options.cacheDir ?? "./cache";
+  options.downloadUrl = options.downloadUrl ?? 'https://dl.nwjs.io';
+  options.manifestUrl = options.manifestUrl ?? 'https://nwjs.io/versions';
+  options.cacheDir = options.cacheDir ?? './cache';
   options.cache = options.cache ?? true;
   options.ffmpeg = options.ffmpeg ?? false;
-  options.logLevel = options.logLevel ?? "info";
+  options.logLevel = options.logLevel ?? 'info';
 
-  if (options.mode === "get") {
+  if (options.mode === 'get') {
     return { ...options };
   }
 
   options.argv = options.argv ?? [];
   options.glob = options.glob ?? true;
-  options.srcDir = options.srcDir ?? (options.glob ? "./*" : ".");
+  options.srcDir = options.srcDir ?? (options.glob ? './*' : '.');
 
-  if (options.mode === "run") {
+  if (options.mode === 'run') {
     return { ...options };
   }
 
-  options.outDir = path.resolve(options.outDir ?? "./out");
+  options.outDir = path.resolve(options.outDir ?? './out');
   options.zip = options.zip ?? false;
 
   options.managedManifest = options.managedManifest ?? false;
@@ -211,7 +205,7 @@ export const parse = async (options, pkg) => {
   options.app.icon = options.app.icon ?? undefined;
 
   // TODO(#737): move this out
-  if (options.platform === "linux") {
+  if (options.platform === 'linux') {
     // linux desktop entry file configurations options
     options.app.genericName = options.app.genericName ?? undefined;
     options.app.noDisplay = options.app.noDisplay ?? undefined;
@@ -235,7 +229,7 @@ export const parse = async (options, pkg) => {
       options.app.prefersNonDefaultGPU ?? undefined;
     options.app.singleMainWindow = options.app.singleMainWindow ?? undefined;
   }
-  if (options.platform === "win") {
+  if (options.platform === 'win') {
     // windows configuration options
     options.app.version = options.app.version ?? pkg.version;
     options.app.comments = options.app.comments ?? undefined;
@@ -254,7 +248,7 @@ export const parse = async (options, pkg) => {
     options.app.languageCode = options.app.languageCode ?? 1033;
   }
 
-  if (options.platform === "osx") {
+  if (options.platform === 'osx') {
     options.app.LSApplicationCategoryType =
       options.app.LSApplicationCategoryType ?? undefined;
     options.app.CFBundleIdentifier =
@@ -276,21 +270,20 @@ export const parse = async (options, pkg) => {
 
 /**
  * Validate options.
- *
  * @param  {import("../index.js").Options} options      Options
  * @param  {object}                        releaseInfo  Version specific NW release info
- * @return {Promise<undefined>}                         Return undefined if options are valid
+ * @returns {Promise<undefined>}                         Return undefined if options are valid
  * @throws {Error}                                         Throw error if options are invalid
  */
 export const validate = async (options, releaseInfo) => {
-  if (!["get", "run", "build"].includes(options.mode)) {
+  if (!['get', 'run', 'build'].includes(options.mode)) {
     throw new Error(
       `Unknown mode ${options.mode}. Expected "get", "run" or "build".`,
     );
   }
-  if (typeof releaseInfo === "undefined") {
+  if (typeof releaseInfo === 'undefined') {
     throw new Error(
-      "Either the specific version info does not exist or the version manifest itself does not exist. In case of the latter, please check your internet connection and try again later.",
+      'Either the specific version info does not exist or the version manifest itself does not exist. In case of the latter, please check your internet connection and try again later.',
     );
   }
   if (!releaseInfo.flavors.includes(options.flavor)) {
@@ -310,40 +303,40 @@ export const validate = async (options, releaseInfo) => {
   // if (typeof options.cacheDir !== "string") {
   //   throw new Error("Expected options.cacheDir to be a string. Got " + typeof options.cacheDir);
   // }
-  if (typeof options.cache !== "boolean") {
+  if (typeof options.cache !== 'boolean') {
     return new Error(
-      "Expected options.cache to be a boolean. Got " + typeof options.cache,
+      'Expected options.cache to be a boolean. Got ' + typeof options.cache,
     );
   }
-  if (typeof options.ffmpeg !== "boolean") {
+  if (typeof options.ffmpeg !== 'boolean') {
     return new Error(
-      "Expected options.ffmpeg to be a boolean. Got " + typeof options.ffmpeg,
+      'Expected options.ffmpeg to be a boolean. Got ' + typeof options.ffmpeg,
     );
   }
 
   if (
-    options.logLevel !== "error" &&
-    options.logLevel !== "warn" &&
-    options.logLevel !== "info" &&
-    options.logLevel !== "debug"
+    options.logLevel !== 'error' &&
+    options.logLevel !== 'warn' &&
+    options.logLevel !== 'info' &&
+    options.logLevel !== 'debug'
   ) {
     throw new Error(
-      "Expected options.logLevel to be 'error', 'warn', 'info' or 'debug'. Got " +
+      'Expected options.logLevel to be \'error\', \'warn\', \'info\' or \'debug\'. Got ' +
       options.logLevel,
     );
   }
 
-  if (options.mode === "get") {
+  if (options.mode === 'get') {
     return undefined;
   }
   if (Array.isArray(options.argv)) {
     return new Error(
-      "Expected options.argv to be an array. Got " + typeof options.argv,
+      'Expected options.argv to be an array. Got ' + typeof options.argv,
     );
   }
-  if (typeof options.glob !== "boolean") {
+  if (typeof options.glob !== 'boolean') {
     return new Error(
-      "Expected options.glob to be a boolean. Got " + typeof options.glob,
+      'Expected options.glob to be a boolean. Got ' + typeof options.glob,
     );
   }
 
@@ -351,7 +344,7 @@ export const validate = async (options, releaseInfo) => {
     await fs.promises.readdir(options.srcDir);
   }
 
-  if (options.mode === "run") {
+  if (options.mode === 'run') {
     return undefined;
   }
 
@@ -360,32 +353,32 @@ export const validate = async (options, releaseInfo) => {
   }
 
   if (
-    typeof options.managedManifest !== "boolean" &&
-    typeof options.managedManifest !== "object" &&
-    typeof options.managedManifest !== "string"
+    typeof options.managedManifest !== 'boolean' &&
+    typeof options.managedManifest !== 'object' &&
+    typeof options.managedManifest !== 'string'
   ) {
     return new Error(
-      "Expected options.managedManifest to be a boolean, object or string. Got " +
+      'Expected options.managedManifest to be a boolean, object or string. Got ' +
       typeof options.managedManifest,
     );
   }
 
-  if (typeof options.managedManifest === "object") {
+  if (typeof options.managedManifest === 'object') {
     if (options.managedManifest.name === undefined) {
-      return new Error("Expected NW.js Manifest to have a `name` property.");
+      return new Error('Expected NW.js Manifest to have a `name` property.');
     }
     if (options.managedManifest.main === undefined) {
-      return new Error("Expected NW.js Manifest to have a `main` property.");
+      return new Error('Expected NW.js Manifest to have a `main` property.');
     }
   }
 
-  if (typeof options.nativeAddon !== "boolean") {
-    if (typeof options.nativeAddon !== "boolean" && typeof options.nativeAddon !== "string") {
-      return new Error("Expected options.nativeAddon to be a boolean or string type. Got " + typeof options.nativeAddon);
+  if (typeof options.nativeAddon !== 'boolean') {
+    if (typeof options.nativeAddon !== 'boolean' && typeof options.nativeAddon !== 'string') {
+      return new Error('Expected options.nativeAddon to be a boolean or string type. Got ' + typeof options.nativeAddon);
     }
 
-    if (semver.parse(options.version).minor >= "83" && options.nativeAddon !== false) {
-      return new Error("Native addons are not supported for NW.js v0.82.0 and below");
+    if (semver.parse(options.version).minor >= '83' && options.nativeAddon !== false) {
+      return new Error('Native addons are not supported for NW.js v0.82.0 and below');
     }
   }
 
@@ -395,39 +388,36 @@ export const validate = async (options, releaseInfo) => {
 
 /**
  * Get path to various NW specific file paths.
- *
  * @async
  * @function
- *
  * @param  {"chromedriver"} type     - NW specific file or directory
  * @param  {object}         options  - nwbuild options
- * @return {string}                  - Path to chromedriver
+ * @returns {string}                  - Path to chromedriver
  * @throws {Error}
  */
 async function getPath(type, options) {
-  if (type === "chromedriver") {
+  if (type === 'chromedriver') {
     return path.resolve(
       options.cacheDir,
-      `nwjs${options.flavor === "sdk" ? "-sdk" : ""}-v${options.version}-${options.platform
+      `nwjs${options.flavor === 'sdk' ? '-sdk' : ''}-v${options.version}-${options.platform
       }-${options.arch}`,
-      `chromedriver${options.platform === "win" ? ".exe" : ""}`,
+      `chromedriver${options.platform === 'win' ? '.exe' : ''}`,
     );
   } else {
-    throw new Error("Invalid type. Expected `chromedriver` but got ", type);
+    throw new Error('Invalid type. Expected `chromedriver` but got ', type);
   }
 }
 
 /**
  * Check if file exists at specified path.
- *
  * @param  {string}           filePath  - File path to check existence of
- * @return {Promise<boolean>}           `true` if exists, otherwise `false`
+ * @returns {Promise<boolean>}           `true` if exists, otherwise `false`
  */
 async function fileExists(filePath) {
   let exists = true;
   try {
     await fs.promises.stat(filePath);
-  } catch (_) {
+  } catch {
     exists = false;
   }
   return exists;
