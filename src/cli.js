6c99f971bdf219835db6f6af129c74ef9902c1c4
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-10-02 12:49:20 +0530

    chore(cli): add basic type definitions

--- src/cli.js
+++ src/cli.js
@@ -5,23 +5,24 @@ import { program } from 'commander';
 import nwbuild from './index.js';
 
 program
-  .option('--mode', 'get, run or build mode')
-  .option('--version', 'NW.js version')
-  .option('--flavor', 'NW.js build flavor')
-  .option('--platform', 'NW.js supported platform')
-  .option('--arch', 'NW.js supported architecture')
-  .option('--downloadUrl', 'NW.js download server')
-  .option('--manifestUrl', 'NW.js version info')
-  .option('--cacheDir', 'Cache NW.js binaries')
-  .option('--outDir', 'NW.js build artifacts')
-  .option('--app', 'Platform specific app metadata. Refer to docs for more info')
-  .option('--cache', 'Flag to enable/disable caching')
-  .option('--ffmpeg', 'Flag to enable/disable downloading community ffmpeg')
-  .option('--glob', 'Flag to enable/disable globbing')
-  .option('--logLevel', 'Specify log level')
-  .option('--zip', 'Flag to enable/disable compression')
-  .option('--managedManifest', 'Managed manifest mode')
-  .option('--nodeAddon', 'Download NW.js Node headers');
+  .argument('<string>', 'File path(s) to project')
+  .option('--mode <string>', 'get, run or build mode')
+  .option('--version <string>', 'NW.js version')
+  .option('--flavor <string>', 'NW.js build flavor')
+  .option('--platform <string>', 'NW.js supported platform')
+  .option('--arch <string>', 'NW.js supported architecture')
+  .option('--downloadUrl <string>', 'NW.js download server')
+  .option('--manifestUrl <string>', 'NW.js version info')
+  .option('--cacheDir <string>', 'Cache NW.js binaries')
+  .option('--outDir <string>', 'NW.js build artifacts')
+  .option('--app <string>', 'Platform specific app metadata. Refer to docs for more info')
+  .option('--cache <boolean>', 'Flag to enable/disable caching')
+  .option('--ffmpeg <boolean>', 'Flag to enable/disable downloading community ffmpeg')
+  .option('--glob <boolean>', 'Flag to enable/disable globbing')
+  .option('--logLevel <string>', 'Specify log level')
+  .option('--zip <string>', 'Flag to enable/disable compression')
+  .option('--managedManifest <string>', 'Managed manifest mode')
+  .option('--nodeAddon <boolean>', 'Download NW.js Node headers');
 
 program.parse();
 
