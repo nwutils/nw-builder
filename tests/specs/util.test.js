56905caf3cf65b19a839b97ad477cefb38ca1d9f
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-11-08 19:40:22 +0530

    chore(util): check for invalid input

--- tests/specs/util.test.js
+++ tests/specs/util.test.js
@@ -5,10 +5,19 @@ import util from '../../src/util.js';
 describe('util/log', function () {
 
   it('shows only error message if log level is error', async function () {
-    expect(util.log('error', 'error', 'Sample message with severity of error')).toBe('[ ERROR ] Sample message with severity of error');
+    expect(util.log('error', 'error', 'Lorem ipsum')).toBe('[ ERROR ] Lorem ipsum');
   });
 
   it('shows only error message if log level is debug', async function () {
-    expect(util.log('debug', 'error', 'Sample message with severity of error')).toBe('');
+    expect(util.log('debug', 'error', 'Lorem ipsum')).toBe('');
   });
+
+  it('throws error if message severity is invalid', async function () {
+    expect(() => util.log('debuggy', 'error', 'Lorem ipsum')).toThrow();
+  });
+
+  it('throws error if user defined log level is invalid', async function () {
+    expect(() => util.log('debug', 'errory', 'Lorem ipsum')).toThrow();
+  });
+  
 });
