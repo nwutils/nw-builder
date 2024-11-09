506e4f2700591ed8b4e522cb91904e2e086e35da
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-11-09 08:06:36 +0530

    fix(util): parse options.app.exec path correctly

--- src/util.js
+++ src/util.js
@@ -243,7 +243,7 @@ export const parse = async (options, pkg) => {
     options.app.notShowIn = options.app.notShowIn ?? undefined;
     options.app.dBusActivatable = options.app.dBusActivatable ?? undefined;
     options.app.tryExec = options.app.tryExec ?? undefined;
-    options.app.exec = options.app.name ?? undefined;
+    options.app.exec = path.resolve(options.app.exec) ?? undefined;
     options.app.path = options.app.path ?? undefined;
     options.app.terminal = options.app.terminal ?? undefined;
     options.app.actions = options.app.actions ?? undefined;
