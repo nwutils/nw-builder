c381ae855bb060920faf9616f14da4d086377764
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-11-24 12:55:58 +0530

    fix(util): correctly validate options.srcDir value

--- src/util.js
+++ src/util.js
@@ -384,7 +384,7 @@ export const validate = async (options, releaseInfo) => {
   if (options.mode === 'get') {
     return undefined;
   }
-  if (typeof options.srcDir !== 'string' && Array.isArray(options.srcDir)) {
+  if (typeof options.srcDir !== 'string' && !Array.isArray(options.srcDir)) {
     throw new Error('Expected options.srcDir to be a string or Array<string>. Got ' + typeof options.srcDir);
   }
   if (!Array.isArray(options.argv)) {
