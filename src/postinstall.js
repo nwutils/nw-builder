14949e9c3a50ad586a72b3b944597309858bc19f
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-03-02 15:36:39 +0530

    chore(postinstall): mitigate command injection

--- src/postinstall.js
+++ src/postinstall.js
@@ -1,5 +1,3 @@
-import child_process from 'node:child_process';
-import fs from 'node:fs';
 import path  from 'node:path';
 import url from 'node:url';
 
@@ -8,6 +6,6 @@ const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const baseVoltaPath = path.resolve(path.join(__dirname, '..', 'node_modules', 'base-volta-off-of-nwjs', 'index.js'));
 
 /* Execute the script in development mode only since it is used during testing */
-if (fs.existsSync(baseVoltaPath)) {
-  child_process.execSync('node ' + baseVoltaPath);
-}
+import(baseVoltaPath)
+  .then(() => console.log('Node version is updated'))
+  .catch((error) => console.log(error));
