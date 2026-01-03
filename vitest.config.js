deeca0c3e0933b0a6829a39d012991febbbeb542
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2026-01-03 14:17:10 +0530

    chore(test): add include and exclude in Vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
+      exclude: ["**/node_modules/**", "**/tests/**"],
+      include: [
+        'src',
+      ],
       provider: 'v8',
       reporter: ['json', 'json-summary'],
       reportOnFailure: true,
