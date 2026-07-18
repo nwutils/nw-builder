499c7a0cc2d10fe22399f2929f396136ce948675
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2026-07-18 16:21:55 +0530

    chore(ci): git ignore /types

--- types/request.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Download from `url` and save at `filePath`.
- * @param {string} url
- * @param {string} filePath
- * @returns {Promise<void>}
- */
-export default function request(url: string, filePath: string): Promise<void>;
