4d7bb3c13bfefdcb60bfb48d46ef8bc48b587557
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2026-09-08 13:49:38 +0530

    feat(packager): add options.format to specify output type

--- packages/packager/src/index.js
+++ packages/packager/src/index.js
@@ -1,7 +1,39 @@
 import appImage from "./appImage.js";
 
-export default {
-  appImage,
-};
+/**
+ * Supported packaged output formats.
+ *
+ * `AppImage`, `deb` and `rpm` package a Linux build. `MSIX` and `NSIS`
+ * package a Windows build. Only `"AppImage"` is implemented today - the
+ * others are reserved so this option doesn't need to change shape once they
+ * land.
+ * @typedef {"AppImage" | "deb" | "rpm" | "msix" | "nsis" | "dmg"} PackageFormat
+ */
 
-export { appImage };
+/**
+ * Package a built NW.js application.
+ * @async
+ * @function
+ * @param  {{format: PackageFormat} & Record<string, any>}  options  `format`, plus the options the matching packager (eg. `appImage`) expects.
+ * @returns {Promise<string>}  Path to the resulting packaged artifact.
+ */
+async function packager({ format, ...options }) {
+  switch (format) {
+    case "AppImage":
+      return appImage(/** @type {any} */ (options));
+    case "deb":
+    case "rpm":
+    case "msix":
+    case "nsis":
+    case "dmg":
+      throw new Error(
+        `options.format ${JSON.stringify(format)} is not implemented yet. Currently only "AppImage" is supported.`,
+      );
+    default:
+      throw new Error(
+        `Expected options.format to be one of "AppImage", "deb", "rpm", "msix", "nsis" or "dmg". Got ${JSON.stringify(format)}.`,
+      );
+  }
+}
+
+export default packager;
